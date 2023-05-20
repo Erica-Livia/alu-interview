e044bb1693549baa8b6dd63d040e3cbbf89bef16
Erica-Livia <e.ingabire1@alustudent.com>
2023-05-20 12:02:38 +0000

    created 0-minoperations.py and a readme.md file

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""minOperation"""
+from math import sqrt
+
+
+def minOperations(n)):
+	if n <= 1:
+		return 0
+	
+	for i in ranfe(2, n+1):
+		if n % i == 0:
+			factor = n // i
