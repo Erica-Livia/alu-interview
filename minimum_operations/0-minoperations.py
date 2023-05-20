8de0d52fe00a65a71fe521cda01ddb4d2c19155f
Erica-Livia <e.ingabire1@alustudent.com>
2023-05-20 12:23:10 +0000

    updated 0-minoperations.py file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,8 +6,8 @@ from math import sqrt
 def minOperations(n):
 	if n <= 1:
 		return 0
-	#list to store the minimum operations for each position
-	min_ops = [float('inf')] * (n+1)
+	#store the minimum operations for each position
+	min_ops = [float('inf')] * (n + 1)
 	min_ops[1] = 0
 	
 	for i in range(2, n+1):
@@ -17,5 +17,8 @@ def minOperations(n):
 
 		for j in range(1, i):
 			min_ops[i] = min(min_ops[i], min_ops[j] + min_ops[i - j])
+
+	if n > 1:
+		min_ops[1] = min_ops[factor] + n
 
 	return min_ops[n] if min_ops[n] != float('inf') else 0
